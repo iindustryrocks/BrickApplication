c0c9aea2d5be1f16bd7047b9e6b12bbf8ed6455e
RicardooCorreia <ricardocorreiap@gmail.com>
2019-05-27 18:33:44 +0100

    Update

--- src/main/java/application/MainApplication.java
+++ src/main/java/application/MainApplication.java
@@ -3,11 +3,16 @@ package application;
 import comms.controller.I2cController;
 import comms.protocol.SensorsTopicsProtocol;
 import lejos.hardware.Button;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.motor.NXTRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import sun.management.Sensor;
 
 public class MainApplication {
 
     public static void main(String[] args) {
-        I2cController i2cComms = new I2cController();
+        /*I2cController i2cComms = new I2cController();
 
         while (Button.ESCAPE.isUp()) {
             int id = Button.waitForAnyPress();
@@ -16,6 +21,12 @@ public class MainApplication {
             }
         }
 
-        i2cComms.close();
+        i2cComms.close();*/
+
+        NXTRegulatedMotor motor = Motor.A;
+        motor.setSpeed(50);
+        while(Button.ESCAPE.isUp()) {
+
+        }
     }
 }
